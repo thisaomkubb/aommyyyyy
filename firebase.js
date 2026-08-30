00cb25d49ab5ed1acf0117a3fcd0d6631011ef33
thisaomkubb <68309010008@atcc.ac.th>
2026-08-30 21:51:53 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+
 import { firebaseConfig } from "./firebase-config.js";
 
-export const firebaseConfigured = !firebaseConfig.apiKey.startsWith("PASTE_");
+export const firebaseConfigured =
+  !firebaseConfig.apiKey.startsWith("PASTE_");
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
