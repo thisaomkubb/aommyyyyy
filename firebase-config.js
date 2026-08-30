dadb30798d4c9963ff83da04b9477fecfd68e235
thisaomkubb <68309010008@atcc.ac.th>
2026-08-30 21:39:11 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,11 +6,20 @@
 // Firebase Web ใช้ modular SDK จาก CDN ตามรูปแบบปัจจุบันของ Firebase Docs.
 // อย่าใส่ service-account private key ลงในไฟล์นี้
 
-export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQDXXy6KioAZ3lRZkQIvP-2Q35JaplmyA",
+  authDomain: "motorcycle-workshop-edac5.firebaseapp.com",
+  projectId: "motorcycle-workshop-edac5",
+  storageBucket: "motorcycle-workshop-edac5.firebasestorage.app",
+  messagingSenderId: "282582818664",
+  appId: "1:282582818664:web:6b6cf0ee94ae3351b600dd"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
